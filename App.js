f6825ac6b6e2c28b879fe4697ced900a72103ede
j1o2h3n4 <johnnatanwillian@hotmail.com>
2018-04-17 23:46:09 -0400

    Added react-native-elements and conditional renders

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { View, Button, TouchableOpacity, StyleSheet, Alert } from 'react-native';
-import { Location, Permissions, MapView} from 'expo';
+import { View, Text, TouchableOpacity, StyleSheet, Alert, TextInput } from 'react-native';
+import { Location, Permissions, MapView } from 'expo';
+import MapViewDirections from 'react-native-maps-directions';
+import { Button } from "react-native-elements";
 
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCyH3WXs70xDF5DrJ72ih-7tTQn1D8CnBw';
 const LATITUDE_DELTA = 0.001;
 const LONGITUDE_DELTA = 0.001;
-
 
 export default class App extends Component {
   state = {
@@ -17,14 +19,20 @@ export default class App extends Component {
     },
     markers: [],
     route: [],
-    geophoning: false
+    geophoning: false,
+    loading_watch_position: false,
+    show_bottom_options: "button",
+
+    markerTitle: "",
+    markerDescription: "",
+    markerError: ""
   };
   
   watchId = null
   
   componentDidMount() {
     this._get_current_position();
-  }
+  };
     
   _get_current_position = async () => {
       navigator.geolocation.getCurrentPosition(
@@ -41,23 +49,8 @@ export default class App extends Component {
       (error) => this.setState({ error: error.message }),
       { enableHighAccuracy: true, timeout: 30000},
     );
-  }
+  };
 
-  _add_marker = async () => {
-    this.setState({
-      markers: [
-        ...this.state.markers, 
-        {
-          coords: {
-            latitude: this.state.mapRegion.latitude,
-            longitude: this.state.mapRegion.longitude
-          },
-          title: "Teste markers",
-          description: 'Marker description'
-        }
-      ]
-    });
-  }
 
   _updateLocation = async (location) => {
     this.setState({
@@ -74,47 +67,136 @@ export default class App extends Component {
         }
       ]
     });
-  }
+  };
 
   _watch_position = async () => {
-    this.watchId =  await Location.watchPositionAsync(
+    this.setState({
+      loading_watch_position: true
+    });
+    this.watchId = await Location.watchPositionAsync(
       options = {enableHighAccuracy: true, distanceInterval: 2},
       callback = currentLocation => {
-        this.setState({geophoning: true})
+        this.setState({geophoning: true});
         this._updateLocation(currentLocation);
       }
     );
   };
 
   _stop_watch_position = async () => {
-    this.watchId.remove();
     this.setState({
-      geophoning: false
+      geophoning: false,
+      loading_watch_position: false
     });
-  }
+    this.watchId.remove();    
+  };
+
+  // CONDITIONAL RENDERS
+
+  _render_marker_option = () => {
+    return (
+      <View>
+        <TextInput
+          placeholder='Título'
+          value={this.state.markerTitle}
+          onChangeText={ text => this.setState({markerTitle: text}) }
+          style={styles.input_style}
+        />
+        <TextInput
+          placeholder='Descrição'
+          value={this.state.markerDescription}
+          onChangeText={ text => this.setState({markerDescription: text}) }
+          style={styles.input_style}          
+        />
+
+        <Text style={styles.error}>{this.state.markerError}</Text>
+
+        <View style={styles.button_container}>
+          <Button 
+            title="Cancelar"
+            onPress={ () => this.setState({show_bottom_options: "button"}) }
+            buttonStyle={styles.button_style}
+          />
+          <Button 
+            title="Inserir"
+            onPress={ () => {
+              if (this.state.markerDescription != "" && this.state.markerTitle != "") {
+                this.setState({
+                  show_bottom_options: "button",
+                  markerError: "",
+                  markers: [
+                    ...this.state.markers, 
+                    {
+                      coords: {
+                        latitude: this.state.mapRegion.latitude,
+                        longitude: this.state.mapRegion.longitude
+                      },
+                      title: "Teste markers",
+                      description: 'Marker description'
+                    }
+                  ]})
+              }
+              else {
+                this.setState({markerError: "Preencha todos os campos."})
+              }
+                 
+              
+              }
+            }
+            buttonStyle={styles.button_style}
+          />
+        </View>  
+      </View>
+    );
+  };
 
   _render_route_button = () => {
     if ( this.state.geophoning ) {
       return (
-        <Button 
-          title="Finalizar Rota"
-          onPress={this._stop_watch_position}
-        />
+        <View style={styles.button_container}>
+          <Button 
+            title="Finalizar Rota"
+            onPress={this._stop_watch_position}
+            buttonStyle={styles.button_style}
+          />
+          <Button 
+            title="Adicionar ponto"
+            onPress={ () => this.setState({show_bottom_options: "marker"}) }
+            buttonStyle={styles.button_style}
+          />
+        </View>
       );
     
     }
 
     else {
       return (
-        <Button 
-          title="Iniciar Rota"
-          onPress={this._watch_position}  
-        />  
+        <View style={styles.button_container}>
+          <Button 
+            title="Iniciar Rota"
+            onPress={this._watch_position}  
+            loading={this.state.loading_watch_position}
+            buttonStyle={styles.button_style}
+          />  
+          <Button 
+            title="Adicionar ponto"
+            onPress={ () => this.setState({show_bottom_options: "marker"}) }
+            buttonStyle={styles.button_style}
+          />
+        </View>
       );
     }
 
-  }
+  };
 
+  _render_bottom_options = () => {
+
+    if (this.state.show_bottom_options === "button") {
+      return this._render_route_button();
+    } 
+    else if (this.state.show_bottom_options === "marker") {
+      return this._render_marker_option();
+    }
+  };
 
   render() {
     return (
@@ -123,36 +205,47 @@ export default class App extends Component {
           style={styles.map}
           region={this.state.mapRegion}
         >
-
+         
           {this.state.markers.map(marker => (
-            <MapView.Marker
-              coordinate={marker.coords}
-              title={marker.title}
-            />
+            <MapView.Callout>
+              <MapView.Marker
+                coordinate={marker.coords}
+                title={marker.title}
+                description={marker.description}
+              />
+            </MapView.Callout>
           ))}
-
-          <MapView.Polyline 
-            coordinates={this.state.route}
-            strokeWidth={6}
-          />
           
+          
+          <MapViewDirections
+            origin={this.state.route[0]}
+            destination={this.state.route.slice(-1)[0]}
+            apikey={GOOGLE_MAPS_APIKEY}
+            waypoints={this.state.route.slice(1, -1)}
+            language="pt-BR"
+            mode="walking"
+            strokeWidth={3}
+            strokeColor="hotpink"
+          />
+
+         
         </ MapView>
         
-        <View style={styles.button_container}>
+        <View>
           
-          {this._render_route_button()}
-
-          <Button 
-            title="Adicionar ponto"
-            onPress={this._add_marker}
-          />
+          {this._render_bottom_options()}
 
         </View>
+
       </View>  
     );
   }
 }
 
+
+
+
+// STYLE
 const styles = StyleSheet.create({
 
   container: {
@@ -160,12 +253,31 @@ const styles = StyleSheet.create({
   },
 
   map: {
-    flex: 1,
+    flex: 1
+  },
+
+  input_style: {
+    height: 44,
+    margin: 3
+  },
+
+  error: {
+    color: "red",
+    textAlign: "center",
+    margin: 5
   },
 
   button_container: {
     flexDirection: "row",
     justifyContent: 'space-around'
-  }
+  },
+
+  button_style: {
+    backgroundColor: "rgba(92, 99,216, 1)",
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 5,
+    margin: 3
+  },
 
 });
