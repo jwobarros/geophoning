d26b5b8bbe96a7d5cd91bd0dca7777cd43d28ee9
Johnnatan William de Oliveira Barros <johnnatan.barros@latam.corp.net>
2018-04-18 23:19:44 -0400

    added marker edit options

--- App.js
+++ App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Alert, TextInput, StatusBar } from 'react-native';
 import { Location, Permissions, MapView } from 'expo';
 import MapViewDirections from 'react-native-maps-directions';
-import { Card, Button, Icon } from "react-native-elements";
-import { EvilIcons } from '@expo/vector-icons';
+import { Card, Button, Icon, FormLabel, FormInput, FormValidationMessage } from "react-native-elements";
 
 const GOOGLE_MAPS_APIKEY = 'AIzaSyCyH3WXs70xDF5DrJ72ih-7tTQn1D8CnBw';
 const LATITUDE_DELTA = 0.001;
@@ -11,6 +10,7 @@ const LONGITUDE_DELTA = 0.001;
 
 export default class App extends Component {
   state = {
+    page: "frontPage",
     error: null,
     mapRegion: { 
       latitude: -15.587265,
@@ -22,11 +22,13 @@ export default class App extends Component {
     route: [],
     geophoning: false,
     loading_watch_position: false,
-    show_bottom_options: "button",
+    show_bottom_options: "",
 
+    markerCounter: 0,
     markerTitle: "",
     markerDescription: "",
-    markerError: ""
+    markerTitleError: "",
+    markerDescriptionError: ""
   };
   
   watchId = null
@@ -54,6 +56,21 @@ export default class App extends Component {
 
 
   _updateLocation = async (location) => {
+
+    var route = this.state.route
+    
+    if ( route.length > 23  ) {
+            
+      for ( var index in route) {
+        if ( index % 2 === 1 ) {
+          route.splice(index, 1)
+        }
+      }
+    
+      this.setState({route: route});
+
+    }
+
     this.setState({
       mapRegion: { 
         latitude: location.coords.latitude, 
@@ -68,6 +85,7 @@ export default class App extends Component {
         }
       ]
     });
+
   };
 
   _watch_position = async () => {
@@ -96,20 +114,13 @@ export default class App extends Component {
   _render_marker_option = () => {
     return (
       <View>
-        <TextInput
-          placeholder='Título'
-          value={this.state.markerTitle}
-          onChangeText={ text => this.setState({markerTitle: text}) }
-          style={styles.input_style}
-        />
-        <TextInput
-          placeholder='Descrição'
-          value={this.state.markerDescription}
-          onChangeText={ text => this.setState({markerDescription: text}) }
-          style={styles.input_style}          
-        />
+        <FormLabel>Título do Ponto</FormLabel>
+        <FormInput onChangeText={ text => this.setState({markerTitle: text}) }/>
+        <FormValidationMessage>{this.state.markerTitleError}</FormValidationMessage>
 
-        <Text style={styles.error}>{this.state.markerError}</Text>
+        <FormLabel>Descrição</FormLabel>
+        <FormInput onChangeText={ text => this.setState({markerDescription: text}) }/>
+        <FormValidationMessage>{this.state.markerDescriptionError}</FormValidationMessage>
 
         <View style={styles.button_container}>
           <Button 
@@ -120,32 +131,64 @@ export default class App extends Component {
           <Button 
             title="Inserir"
             onPress={ () => {
-              if (this.state.markerDescription != "" && this.state.markerTitle != "") {
-                this.setState({
-                  show_bottom_options: "button",
-                  markerError: "",
-                  markers: [
-                    ...this.state.markers, 
+                if (this.state.markerDescription != "" && this.state.markerTitle != "") {
+                  this.setState({markerCounter: this.state.markerCounter+1});
+                  this.setState(
                     {
-                      coords: {
-                        latitude: this.state.mapRegion.latitude,
-                        longitude: this.state.mapRegion.longitude
-                      },
-                      title: this.state.markerTitle,
-                      description: this.state.markerDescription
-                    }
-                  ]})
-              }
-              else {
-                this.setState({markerError: "Preencha todos os campos."})
-              }
-                 
-              
+                    show_bottom_options: "button",
+                    markerError: "",
+                    markers: [
+                      ...this.state.markers, 
+                      {
+                        coords: {
+                          latitude: this.state.mapRegion.latitude,
+                          longitude: this.state.mapRegion.longitude
+                        },
+                        title: this.state.markerTitle,
+                        description: this.state.markerDescription,
+                        key: this.state.markerCounter
+                      }
+                    ]}
+                  );
+                }
+
+                if ( this.state.markerDescription == "" ) {
+                  this.setState({markerDescriptionError: "Este campo é obrigatório."})
+                }
+
+                if ( this.state.markerTitle == "" ) {
+                  this.setState({markerTitleError: "Este campo é obrigatório."})
+                }
+
               }
             }
             buttonStyle={styles.button_style}
           />
         </View>  
+      </View>
+    );
+  };
+
+  _render_marker_edit_option = () => {
+    return (
+      <View>
+
+        <FormLabel>Título do Ponto</FormLabel>
+        <FormInput value={this.state.markerTitle} onChangeText={ text => this.setState({markerTitle: text}) }/>
+        <FormValidationMessage>{this.state.markerTitleError}</FormValidationMessage>
+
+        <FormLabel>Descrição</FormLabel>
+        <FormInput value={this.state.markerDescription} onChangeText={ text => this.setState({markerDescription: text}) }/>
+        <FormValidationMessage>{this.state.markerDescriptionError}</FormValidationMessage>
+
+        <View style={styles.button_container}>
+          <Button 
+            title="Cancelar"
+            onPress={ () => this.setState({show_bottom_options: "button"}) }
+            buttonStyle={styles.button_style}
+          />
+        </View>  
+
       </View>
     );
   };
@@ -175,6 +218,12 @@ export default class App extends Component {
       return (
         <View style={styles.button_container}>
           <Button 
+            title="Voltar"
+            onPress={ () => this.setState({page: "frontPage", show_bottom_options: ""}) }
+            buttonStyle={styles.button_style}
+            icon={ {name: "times", type: "font-awesome"} }
+          />
+          <Button 
             title="Iniciar Rota"
             onPress={this._watch_position}  
             loading={this.state.loading_watch_position}
@@ -192,13 +241,22 @@ export default class App extends Component {
     if (this.state.show_bottom_options === "button") {
       return this._render_route_button();
     } 
+
     else if (this.state.show_bottom_options === "marker") {
       return this._render_marker_option();
+    }
+
+    else if (this.state.show_bottom_options === "editMarker") {
+      return this._render_marker_edit_option();
+    }
+
+    else {
+      return null;
     }
   };
 
   _render_map = () => {
-    return (        
+    return (
       <MapView
         style={styles.map}
         region={this.state.mapRegion}
@@ -213,9 +271,17 @@ export default class App extends Component {
         {this.state.markers.map(marker => (
           <MapView.Callout>
             <MapView.Marker
+              key={marker.key}
               coordinate={marker.coords}
               title={marker.title}
               description={marker.description}
+              onPress={() => {
+                  this.setState({
+                    show_bottom_options: 'editMarker', 
+                    markerTitle: marker.title, 
+                    markerDescription: marker.description});
+                }
+              }
             />
           </MapView.Callout>
         ))}
@@ -231,23 +297,20 @@ export default class App extends Component {
           strokeWidth={3}
           strokeColor="hotpink"
         />      
-      </ MapView>);
+      </ MapView>
+    );
   };
 
-  render() {
+  _render_front_page = () => {
     return (
-      <View style={styles.container}>
-
-        <StatusBar
-          backgroundColor="rgba(255, 255, 255, 1)"
-          barStyle="light-content"
-          animated={true}
-        />
-
-        <Card title="NASCENTES DO XINGU" style={{flex:1}}>
+      <Card 
+      title="NASCENTES DO XINGU" 
+      containerStyle={{flex: 1, marginBottom: 15}}
+      >
+        <View style={{marginTop: 150}}>
           <Button 
             title="Iniciar"
-            onPress={() => none}
+            onPress={() => this.setState({page: "map", show_bottom_options: "button"})}
             buttonStyle={styles.button_style}
           />
           <Button 
@@ -255,14 +318,33 @@ export default class App extends Component {
             onPress={() => none}
             buttonStyle={styles.button_style}
           />
-          <View style={styles.footer}>
-            <Text>Copyright</Text>
-            <Icon name='copyright' />
-            <Text>2018 - Nascentes do Xingu</Text>
-          </View>
-        </Card>
+        </View>
+        <View style={styles.footer}>
+          <Icon name='copyright' />
+          <Text>2018 - Nascentes do Xingu</Text>
+        </View>
+      </Card>
+      
+    );
+  };
 
-      </View>  
+  _render_page = () => {
+    if (this.state.page === "frontPage") {
+      return this._render_front_page();
+    } 
+    else if (this.state.page === "map") {
+      return this._render_map();
+    }
+  }; 
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        {this._render_page()}
+        {this._render_bottom_options()}
+
+      </View>
     );
   }
 }
@@ -280,26 +362,12 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(255, 255, 255, 0.6)", 
   },
 
-  topMenu: {
-    flexDirection:'row',
-    alignItems:'center',
-    justifyContent:'center',
-    flexDirection: "row",
-    justifyContent: "space-around", 
-    margin: 5   
-  },
-
-  topMenuText: {
-    fontWeight:'bold',
-    textAlign:'center',
-    fontSize:20,
-  },
-
   footer: {
-    flexDirection: "row",
-    alignItems:'center',
-    justifyContent:'center',
-    alignItems: "center"
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 150
   },
 
   map: {
